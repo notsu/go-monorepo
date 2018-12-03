133c3fb35c3317edf16a15ae1fc34e25334f6132
Pichet Itngam <pichet.i@linecorp.com>
2018-12-03 17:15:30 +0700

    fix: try anti-pattern for go path

--- services/user/go.mod
+++ services/user/go.mod
@@ -1,3 +1,1 @@
-module github.com/notsu/gomono/services/user
-
-require github.com/notsu/gomono/services/post v0.0.0-20181203094246-bc9985ebc58b
+module github.com/notsu/gomono/user
--- services/user/main.go
+++ services/user/main.go
@@ -2,11 +2,10 @@ package main
 
 import (
 	"fmt"
-
-	hello "github.com/notsu/gomono/services/post/hello"
+	// hello "github.com/notsu/gomono/services/post/hello"
 )
 
 func main() {
 	fmt.Println("Hello from USER")
-	fmt.Println(hello.Hello())
+	// fmt.Println(hello.Hello())
 }
